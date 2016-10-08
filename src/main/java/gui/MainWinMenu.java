b55027a33a3465450393c6f95eb97d88c7c6f1e3
Lasse Osterhagen <l.osterhagen@gmail.com>
2016-10-09 00:45:07 +0200

    Removed unnecessary generics.

--- src/main/java/gui/MainWinMenu.java
+++ src/main/java/gui/MainWinMenu.java
@@ -13,7 +13,7 @@ import static gui.Util.getKeyCodeFromString;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Optional;
-import java.util.function.Supplier;
+
 
 public class MainWinMenu {
 	
@@ -58,29 +58,27 @@ public class MainWinMenu {
 		menu = new JMenu(getGUIText("exercise"));
 		menu.setMnemonic(getKeyCodeFromString(getGUIText("exerciseMnemonic")));
 		menuBar.add(menu);
-		addItem(menu, JMenuItem::new, "select", "selectMnemonic");
+		addItem(menu, new JMenuItem(), "select", "selectMnemonic");
 		
 		// Second column
 		menu = new JMenu(getGUIText("keyboard"));
 		menu.setMnemonic(getKeyCodeFromString(getGUIText("keyboardMnemonic")));
 		menuBar.add(menu);
-		addItem(menu, JCheckBoxMenuItem::new, "showKeyboard", "showKeyboardMnemonic");
+		addItem(menu, new JCheckBoxMenuItem(), "showKeyboard", "showKeyboardMnemonic");
 	}
 	
 	
 	/**
 	 * Adds a <code>JMenuItem</code> to a <code>JMenu</code> (which is a column of a
-	 * <code>JMenuBar</code>
+	 * <code>JMenuBar</code>).
 	 * @param menu the menu column to which the item should be added
-	 * @param sp Supplier for the kind of JMenuItem to be created
+	 * @param item the JMenuItem that should be added
 	 * @param commandKey fulfills two tasks: First, it is the key for I18n of the item text
 	 * (<i>guiText.properties</i>); second, it serves as key in the switch statement of the
 	 * <code>MenuActionListener</code>.
 	 * @param mnemonicKey key for I18n of the mnemonic
 	 */
-	private <T extends JMenuItem>
-		void addItem(JMenu menu, Supplier<T> sp, String commandKey, String mnemonicKey) {
-		T item = sp.get();
+	private void addItem(JMenu menu, JMenuItem item, String commandKey, String mnemonicKey) {
 		item.setText(getGUIText(commandKey));
 		item.setMnemonic(getKeyCodeFromString(getGUIText(mnemonicKey)));
 		item.setActionCommand(commandKey);
